6e2a7df183b951fabb0aeb3b4bbf310f9f478824
chseong <64356689+chseong02@users.noreply.github.com>
2025-07-07 10:37:51 +0900

    ADD: score page main view

---
+++ PageTurner/View/ScorePageMainView.swift
@@ -0,0 +1,54 @@
+//
+//  ScorePageMainView.swift
+//  PageTurner
+//
+//  Created by chseong on 7/4/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ScorePageMainView: View {
+    @State var isShownSheet: Bool = false
+    
+    var body: some View {
+        VStack {
+            Text("아직 악보가 없네요!\n새 악보를 추가해주세요")
+                .font(.title)
+                .multilineTextAlignment(.center)
+            //Button("악보 추가").buttonStyle(style: .bordered)
+            Button{
+                isShownSheet = true
+            }
+            label: {
+                Text("악보 추가").font(.headline)
+            }.buttonStyle(.borderedProminent)
+        }
+        .navigationTitle(Text("Page Turner"))
+        .toolbarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem (placement: .navigationBarTrailing){
+                Button {
+                } label: {
+                    Image(systemName: "book.pages")
+                }
+
+            }
+        }
+//        .fullScreenCover(isPresented: $isShownSheet) {
+//            NewScorePage(isPresented: $isShownSheet)
+//        }
+        .sheet(isPresented: $isShownSheet) {
+            NewScorePage(isPresented: $isShownSheet)
+        }
+    }
+}
+
+#Preview {
+    let container = try! ModelContainer(
+      for: Score.self, Item.self,
+      configurations: ModelConfiguration(isStoredInMemoryOnly: true)
+    )
+    ContentView(modelContext: container.mainContext)
+        .modelContainer(for: Item.self, inMemory: true)
+}
